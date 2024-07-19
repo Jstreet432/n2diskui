bf93e1be0acb32014871a0ae8275a68565b6eab3
Jstreet432 <jstreet0951@gmail.com>
2024-07-18 21:05:38 -0400

    temp work on daterange picker

--- n2diskui/static/js/filter_submit.js
+++ n2diskui/static/js/filter_submit.js
@@ -1,4 +1,7 @@
 import Swal from "sweetalert2";
+import moment from "moment";
+import 'daterangepicker';
+
 window.Swal = Swal;
 
 export function popUpSuccess(fileDownloadLoc){
@@ -51,3 +54,19 @@ function verifyDataBeforeSubmit(event) {
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('filter_button').addEventListener('click', verifyDataBeforeSubmit);
 });
+
+export function buildDateRangePicker(){
+    const datetimeWidget = document.querySelector("input[name='datetimerange']");
+    if (datetimeWidget) {
+        new DateRangePicker(datetimeWidget, {
+            timepicker: true,
+            timePicker24Hour: true,
+            startDate: moment().startOf('hour'),
+            endDate: moment().startOf('hour').add(1 , 'hour'),
+            locale: {
+                format: 'YYYY-MM-DD HH:mm:ss'
+            }
+
+        });
+    }
+}
